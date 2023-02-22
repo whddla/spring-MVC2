acd00c965410133cb027d76ad0b8a76c39ff4c3a
whddla <whddla8912@gmail.com>
2023-02-22 16:25:10 +0900

    update route

--- spring-MVC2/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ spring-MVC2/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,14 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -43,10 +46,25 @@ public class BasicController {
         return "basic/variable";
     }
 
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+
     @Data
     static class User {
         private String username;
-        private int age; public User(String username, int age) {
+        private int age;
+
+        public User(String username, int age) {
             this.username = username;
             this.age = age;
         }
